dcacf64ae1e60802fd01fcf27dc98f3d6bce2c54
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-07-26 13:18:33 -0400

    questions 1-4

--- main.js
+++ main.js
@@ -2,33 +2,56 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // Then, write and example of using the function.
 
-function max(){
+function max(num1,num2){
     // Your answer here
+    if(num1 > num2){
+      return num1;
+    }
+    else{
+      return num2;
+    }
 }
-
+console.log(max (74,78));
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function maxOfThree(){
+function maxOfThree(num1, num2,num3){
     // Your answer here
+    if (num1 > num2){
+      return num1;
+    }
+    else if(num2 > num3){
+      return num2;
+    }
+    else if{
+      return num3;
+    }
 }
 
-
+console.log(maxOfThree(32,45,12));
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // Then, write and example of using the function.
 
 function isVowel(char){
     // Your answer here
+    if (char=== "a"||"e"||"i"||"o"||"u"||"y"){
+      return true;
+    }
+      else{
+        false;
+    }
 }
-
+console.log(isVowel("b"));
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
-
+function sum(num1,num2){
+  return num1 + num2
+}
 
 
 // 5.
